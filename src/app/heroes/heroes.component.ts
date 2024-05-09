436ba1ea72645954c25e0d35628101fb5d773b48
JISA8SS <jackson.newman@jbhunt.com>
2024-05-09 15:00:38 -0500

    finished step 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Hero } from "../hero";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
-import {HEROES} from "../mock-heroes";
 import {NgFor} from "@angular/common";
 import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
+import {HeroService} from "../hero.service";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -14,9 +15,25 @@ import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  heroes = HEROES;
+
   selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
 }
